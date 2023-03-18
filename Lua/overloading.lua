71ecfb8e9b29ceb856d6021f842e452eb779bb59
Okafor Ifeanyi <zeusifeanyi058@gmail.com>
2023-03-18 20:53:47 +0100

    fix: Readme refrencing / Debugging

--- Lua/overloading.lua
+++ Lua/overloading.lua
@@ -10,4 +10,17 @@ function test(x, y, z)
   
   test("hello", "world")
   test("hello", "world", "Lua")
+ 
   
+--  Lua believes that the function is being redefined and will only use the most recently defined function.
+
+function add(one, two)
+    print(one+two)
+end
+    
+function add(one, two, three)
+    print(one+two+three)
+end
+      
+print(add(5,3)) --input:6: attempt to perform arithmetic on a nil value (local 'three')
+    
